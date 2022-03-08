5abe5c9a6826cf6a0c9a9dad45db0cf470361297
koussayb6 <mohamedkoussay.boussetta@esprit.tn>
2022-03-09 00:35:21 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const colors = require('colors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
