691708226c3b09cfd16162a5eea386e02179a723
koussayb6 <mohamedkoussay.boussetta@esprit.tn>
2022-03-08 23:55:06 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+// Serve frontend
+/*if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}*/
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
